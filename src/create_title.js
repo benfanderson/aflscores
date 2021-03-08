e9172f54c6c4a96608a6c16fd4a26a0b5e37d3e3
benfanderson <benfanderson81@gmail.com>
2021-03-08 15:20:40 +0800

    basic layouts for club and round list pages

--- src/create_title.js
+++ src/create_title.js
@@ -1,10 +1,3 @@
-function createTitle(param) {
-  if (Number.isInteger(param)) {
-    const title = ` Round ${param}`;
-    return title;
-  }
-  const title = param;
-  return title;
-}
+const createTitle = (param) => (Number.isInteger(param) ? `Round ${param}` : param);
 
 export default createTitle;
