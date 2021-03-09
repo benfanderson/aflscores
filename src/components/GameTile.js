5fe00995b6ff84f9f5fb4e0555215f916afc807f
benfanderson <benfanderson81@gmail.com>
2021-03-09 19:55:52 +0800

    Winner now listed first once game is completed

--- src/components/GameTile.js
+++ src/components/GameTile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import clubLogo from '../club_logo';
+import gameWinner from '../game_winner';
 import styles from '../../public/styles.css';
 
 function GameTile(props) {
@@ -12,7 +13,7 @@ function GameTile(props) {
     <div className={styles.gameTile}>
       <img src={clubLogo(game.hteam)} alt={`${game.hteam} logo`} />
       <div className={styles.gameInfo}>
-        <p>{game.complete === 0 ? `${game.hteam} v ${game.ateam}` : `${game.hteam} ${game.hscore} ${game.ateam} ${game.hscore}` }</p>
+        <p>{game.complete === 0 ? `${game.hteam} v ${game.ateam}` : game.complete < 100 ? `${game.hteam} ${game.hscore} ${game.ateam} ${game.hscore}` : gameWinner(game) }</p>
         <p>{game.venue}</p>
         <p>{game.complete === 0 ? `${gameTime}` : game.complete < 100 ? <span className={styles.live}>LIVE</span> : null}</p>
       </div>
