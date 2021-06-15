446f41224897375221af77010f856f85f770b70c
benfanderson <benfanderson81@gmail.com>
2021-06-15 12:34:10 +0800

    Added front link

--- src/game_winner.js
+++ src/game_winner.js
@@ -1,3 +1,3 @@
-const gameWinner = (game) => (game.hscore > game.ascore ? `${game.hteam} ${game.hgoals}.${game.hbehinds} ${game.hscore} def ${game.ateam} ${game.agoals}.${game.abehinds} ${game.ascore} ` : `${game.ateam} ${game.agoals}.${game.abehinds} ${game.ascore} def ${game.hteam} ${game.hgoals}.${game.hbehinds} ${game.hscore} `);
+const gameWinner = (game) => (game.hscore > game.ascore ? `${game.hteam} ${game.hgoals}.${game.hbehinds} ${game.hscore} def ${game.ateam} ${game.agoals}.${game.abehinds} ${game.ascore}` : `${game.ateam} ${game.agoals}.${game.abehinds} ${game.ascore} def ${game.hteam} ${game.hgoals}.${game.hbehinds} ${game.hscore}`);
 
 export default gameWinner;
--- src/tests/club_logo.test.js
+++
@@ -1,7 +0,0 @@
-import clubLogo from '../club_logo';
-
-describe('clubLogo function', () => {
-  it('should return string URL to club logo svg', () => {
-    expect(clubLogo('Essendon')).toEqual('../public/images/Essendon.svg');
-  });
-});
