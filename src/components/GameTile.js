fced80a3f0bf02456d1ae304bee6c8d6a3934ea0
benfanderson <benfanderson81@gmail.com>
2021-06-19 16:30:33 +0800

    Removed stray $ from live game score

--- src/components/GameTile.js
+++ src/components/GameTile.js
@@ -34,7 +34,7 @@ function GameTile(props) {
             {game.ateam}
             {' '}
             {game.agoals}
-            .$
+            .
             {game.abehinds}
             {' '}
             {game.ascore}
