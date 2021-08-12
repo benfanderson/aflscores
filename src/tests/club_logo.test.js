06ece1d81d25a9b70a4d3f3b140f9f31f2662c11
benfanderson <benfanderson81@gmail.com>
2021-08-12 16:24:43 +0800

    Club logo test created

--- src/tests/club_logo.test.js
+++ src/tests/club_logo.test.js
@@ -1,7 +1,8 @@
 import clubLogo from '../club_logo';
+import Essendon from '../../public/images/Essendon.svg';
 
 describe('clubLogo function', () => {
   it('should display the appropriate team logo based on the club parameter', () => {
-    expect(clubLogo('Essendon')).toBe('../public/images/Essendon.svg');
+    expect(clubLogo('Essendon')).toEqual(Essendon);
   });
 });
